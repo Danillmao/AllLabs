3956c024184b558d0fda3bd4d426d8002c248d6b
PIBO0121033 Морозов Данил Павлович <PIBO0121033@sibupk.su>
2022-12-10 10:01:59 +0300

    Lmao

--- LR5/LR5/Program.cs
+++ LR5/LR5/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LR5;
+using System;
 using System.Text.RegularExpressions;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -9,22 +10,7 @@ namespace MyApp
 
         static void Main(string[] args)
         {
-            string path = "C:\\Labs\\AllLabs\\LR5\\LR5\\AllValues.txt"; 
-            
-            string[] input = File.ReadAllLines(path); //чтение массива строк
-            for (int i = 0; i < input.Length; ++i)
-            {
-                Regex regex = new Regex(@"([A-Z]|[a-z])+\s([A-Z]|[a-z])+\s([A-Z]|[a-z])+\W\s(\d){1,}\s([A-Z]|[a-z])+(\W)\s(g)(\W)([A-Z]|[a-z])+\W");
-                if (regex.IsMatch(input[i])) 
-                {
-                    Console.WriteLine(regex.Match(input[i]));
-                    Console.WriteLine(regex.Match(@"^"));
-                }
-                else
-                {
-                    Console.WriteLine("Не найдено");
-                }
-            }  
+            Zadanie.Z4();
         }
     }
 }
@@ -35,5 +21,7 @@ A)@"^[a]$"
 B)@"[a]{4,}[^ ]"
 C)@"[a]{1}\s[a]{2}\s[a]{1}"
 2.@"(\d|[a-z]){5,}"
-3.@"[a-z]+[^ ]\W[^ ][a-z]+[^ ]\W[a-z]+"
+3.@"[a-z]+[^ ][@]{1}[^ ][a-z]+[^ ]\W[a-z]+"
+4.
+C)@"([A-Za-z])+\s([A-Za-z])+\s([A-Za-z])+[,]\s([0-9]+)\s([A-Za-z])+[,]\s[g][.]([A-Za-z])+[.]"
  */
---
+++ LR5/LR5/Zadanie.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace LR5
+{
+    internal class Zadanie
+    {
+        public static void Z1()
+        {
+            //1.Написать регулярное выражение, которые проверят строки на соответствие
+
+            string path = "AllValues.txt";
+            string[] input = File.ReadAllLines(path); //чтение массива строк
+            for (int i = 0; i < input.Length; ++i)
+            {
+                Regex regex = new Regex(@"^[a]$");
+                if (regex.IsMatch(input[i]))
+                {
+                    Console.WriteLine(regex.Match(input[i]));
+
+                }
+                else
+                {
+                    // Console.WriteLine("Не найдено");
+                }
+            }
+            string[] input1 = File.ReadAllLines(path); //чтение массива строк
+            for (int i = 0; i < input.Length; ++i)
+            {
+                Regex regex = new Regex(@"[a]{4,}[^ ]");
+                if (regex.IsMatch(input1[i]))
+                {
+                    Console.WriteLine(regex.Match(input[i]));
+
+                }
+                else
+                {
+                    // Console.WriteLine("Не найдено");
+                }
+            }
+            string[] input2 = File.ReadAllLines(path); //чтение массива строк
+            for (int i = 0; i < input.Length; ++i)
+            {
+                Regex regex = new Regex(@"[a]{1}\s[a]{2}\s[a]{1}");
+                if (regex.IsMatch(input1[i]))
+                {
+                    Console.WriteLine(regex.Match(input[i]));
+
+                }
+                else
+                {
+                    // Console.WriteLine("Не найдено");
+                }
+            }
+        }
+        //2.Написать регулярное выражение, заставляющее вводить не менее 5 алфавитно - цифровых символов.
+        public static void Z2()
+        {
+            string path = "AllValues.txt";
+
+            string[] input = File.ReadAllLines(path); //чтение массива строк
+            for (int i = 0; i < input.Length; ++i)
+            {
+                Regex regex = new Regex(@"(\d|[a-z]){5,}");
+                if (regex.IsMatch(input[i]))
+                {
+                    Console.WriteLine(regex.Match(input[i]));
+
+                }
+                else
+                {
+                    // Console.WriteLine("Не найдено");
+                }
+            }
+
+        }
+        //3.Написать регулярное выражение, которое проверят email простого вида
+        public static void Z3()
+        {
+            string path = "AllValues.txt";
+
+            string[] input = File.ReadAllLines(path); //чтение массива строк
+            for (int i = 0; i < input.Length; ++i)
+            {
+                Regex regex = new Regex(@"[a-z]+[^ ][@]{1}[^ ][a-z]+[^ ]\W[a-z]+");
+                if (regex.IsMatch(input[i]))
+                {
+                    Console.WriteLine(regex.Match(input[i]));
+
+                }
+                else
+                {
+                    // Console.WriteLine("Не найдено");
+                }
+            }
+        }
+        //4.Выполните одно из заданий, приведенных ниже.
+        public static void Z4()
+        {
+            string path = "AllValues.txt";
+
+            string[] input = File.ReadAllLines(path); //чтение массива строк
+            for (int i = 0; i < input.Length; ++i)
+            {
+                Regex regex = new Regex(@"([A-Za-z])+\s([A-Za-z])+\s([A-Za-z])+[,]\s([0-9]+)\s([A-Za-z])+[,]\s[g][.]([A-Za-z])+[.]");
+                if (regex.IsMatch(input[i]))
+                {
+                    Console.WriteLine(regex.Match(input[i]));
+                }
+                else
+                {
+                    // Console.WriteLine("Не найдено");
+                }
+            }
+        }
+    }
+}
