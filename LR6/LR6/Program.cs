e0ed88d157ced55bd2d893c6454051a1a4f17bd5
PIBO0121033 Морозов Данил Павлович <PIBO0121033@sibupk.su>
2023-03-25 20:59:51 +0700

    Гит сломан

--- LR6/LR6/Program.cs
+++ LR6/LR6/Program.cs
@@ -11,6 +11,7 @@ namespace MyApp
             Integrator i1 = new Integrator(e); //создаем интегратор для этого уравнения
             double integrValue = i1.Integrate(10, 30); //вызываем интегрирование на интвервале 10;30
             Console.WriteLine(integrValue);
+            Console.WriteLine(integrValue);
             //создаем интегратор для другого уравнения:
             Integrator i2 = new Integrator(new QuadEquation(-3, 0, 2.3));
             integrValue = i2.Integrate(-2, 100);    //вызываем ф-ю интегрирования
--- LR6WF/LR6WF/Form1.Designer.cs
+++ LR6WF/LR6WF/Form1.Designer.cs
@@ -121,7 +121,7 @@
             "quadEquation",
             "secondEquation",
             "thirdEquation"});
-            this.comboBox1.Location = new System.Drawing.Point(688, 136);
+            this.comboBox1.Location = new System.Drawing.Point(688, 159);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(100, 21);
             this.comboBox1.TabIndex = 15;
